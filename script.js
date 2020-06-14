7c86c833a80bbc480507810bf15a088ec56632bb
YuraVasylyshyn <yura.vas535@gmail.com>
2020-06-15 02:52:23 +0300

    'asdasd'

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-function makeOrder() {
-    const order = document.querySelector(`#btn`);
-    order.classList.toggle("order-button");
-    document.querySelector(".fullstar1").classList.toggle("star");
-    document.querySelector(".fullstar2").classList.toggle("star");
-    document.querySelector(".fullstar3").classList.toggle("star");
-    document.querySelector(".fa-star-half-o").classList.toggle("star");
-    document.querySelector(".fa-star-o").classList.toggle("star");
 
-    const span = document.querySelector(".dto-btn-span").innerText;
-    if (span == `ORDER`){
-      document.querySelector(".dto-btn-span").innerText = `CANCEL`;
-      document.querySelector(".dto-btn-span").classList.add("black");
+    function makeOrder(param) { 
+      var block = param.closest('.dishes-title-order'); 
+      var i = block.getElementsByTagName('i'); 
+      for (let count = 0; count < i.length; count++) { 
+        i[count].classList.toggle('star') 
+      } 
+      param.classList.toggle("order-button"); 
+    
+
+    const span = param.firstElementChild;
+    if (span.innerText == `ORDER`){
+      span.innerText = `CANCEL`;
+      span.classList.add("black");
     } else {
-      document.querySelector(".dto-btn-span").innerText = `ORDER`;
-      document.querySelector(".dto-btn-span").classList.remove("black");
+      span.innerText = `ORDER`;
+      span.classList.remove("black");
     }
      ;
 }
