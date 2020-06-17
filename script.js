9663185da6357782ba57b39c6738ad5c90fac35c
YuraVasylyshyn <yura.vas535@gmail.com>
2020-06-17 23:39:51 +0300

    first commit

--- script.js
+++ script.js
@@ -19,6 +19,26 @@
      ;
 }
 
+function hover(param){
+  var block = param.closest('.work-blank'); 
+  var i = block.getElementsByClassName('hovernone');
+  for (let count = 0; count < i.length; count++) {
+    i[count].classList.add('hover')
+    document.querySelector('.work-blank').classList.add('hover-blank')
+    document.querySelector('.work-lines').classList.add('hover-lines')
+  }
+}
+
+function noHover(param){
+  var block = param.closest('.work'); 
+  var i = block.getElementsByClassName('hovernone');
+  for (let count = 0; count < i.length; count++) {
+    i[count].classList.remove('hover')
+    document.querySelector('.work-blank').classList.remove('hover-blank')
+    document.querySelector('.work-lines').classList.remove('hover-lines')
+  }
+}
+
 
 
     
